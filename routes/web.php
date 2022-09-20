7419a9a89e80b5818122fa9bf605841de4fe11f7
Zura Moon <zuramoon01@gmail.com>
2022-09-20 18:08:04 +0700

    Update web route to add login user proses and logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Models\AuthorizationType;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index');
+Route::view('/', 'index')->name('dashboard');
 
-Route::get('/login', function () {
-    return view('login.index');
-});
+Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
+Route::post('/login', [AuthController::class, 'loginUser']);
+Route::get('/logout', [AuthController::class, 'logoutUser']);
