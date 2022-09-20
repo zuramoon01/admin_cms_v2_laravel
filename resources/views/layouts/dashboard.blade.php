03036db9b05721ec7ff255670d61d762a044f5c5
Zura Moon <zuramoon01@gmail.com>
2022-09-21 04:51:56 +0700

    Add meata tag and js yield to layouts dashboard view

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>SB Admin 2 - Blank</title>
 
@@ -199,6 +200,8 @@
             </div>
         </div>
     </div>
+
+    @yield('js')
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
