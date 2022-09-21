89f0a0261ea95b593804376333fe6c9e9f21c0ee
ZuraMoon <zuramoon01@gmail.com>
2022-09-21 11:39:16 +0700

    Delete unique from name because it make the order not by id anymore

--- database/migrations/2022_09_20_103932_create_authorization_types_table.php
+++ database/migrations/2022_09_20_103932_create_authorization_types_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('authorization_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->unique();
+            $table->string('name', 50);
         });
 
         if (Schema::hasTable('authorizations') && !(Schema::hasColumn('authorizations', 'role_id'))) {
