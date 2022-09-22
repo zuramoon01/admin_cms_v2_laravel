3c944467883cab0c6cd25ba94ac1366d715eb526
Zura Moon <zuramoon01@gmail.com>
2022-09-22 11:42:48 +0700

    Revert "Create table component"
    This reverts commit 59012e65240f039c836d6a63070c97c5dc674d45.

---
+++ resources/views/components/table/index.blade.php
@@ -0,0 +1,25 @@
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800">Tables {{ $heading }}</h1>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    {{ $title }}
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <colgroup>
+                    {{ $colgroup }}
+                </colgroup>
+                <thead>
+                    <tr>
+                        {{ $head }}
+                    </tr>
+                </thead>
+                <tbody>
+                    {{ $slot }}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
