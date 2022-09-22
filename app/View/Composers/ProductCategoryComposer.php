96b971a0b4ef9ddfb6a5f9e00ac1578244f192e8
Zura Moon <zuramoon01@gmail.com>
2022-09-22 11:26:15 +0700

    Add heading and formInputs to view product-category and add logic if view product-category is index or form

--- app/View/Composers/ProductCategoryComposer.php
+++ app/View/Composers/ProductCategoryComposer.php
@@ -9,22 +9,41 @@ use App\Models\ProductCategory;
 
 class ProductCategoryComposer
 {
+    protected $heading;
     protected $colSizes;
     protected $titles;
     protected $productCategories;
+    protected $formInputs;
 
     public function __construct()
     {
+        $this->heading = "Product Category";
         $this->colSizes = [1, 3, 7, 1];
         $this->titles = Schema::getColumnListing('product_categories');
         $this->productCategories = ProductCategory::all();
+        $this->formInputs = [
+            [
+                "name" => 'category',
+                'label' => 'Category',
+                'type' => 'text',
+            ],
+            [
+                "name" => 'description',
+                'label' => 'Description',
+                'type' => 'textarea',
+            ],
+        ];
 
         array_shift($this->titles);
     }
 
     public function compose(View $view)
     {
+        $viewName = explode(".", $view->name());
+        $viewType = $viewName[count($viewName) - 1];
+
         $view->with([
+            'heading' => $this->heading,
             'colSizes' => $this->colSizes,
             'titles' => $this->titles,
             'productCategories' => $this->productCategories,
