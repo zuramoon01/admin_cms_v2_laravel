0547d38f3edb7905ff2a5a89e2f7258579895a6b
ZuraMoon <zuramoon01@gmail.com>
2022-09-23 09:41:28 +0700

    Fix view that not dynamic

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,24 +2,7 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Login</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
+    <x-partials._head-content />
 </head>
 
 <body class="bg-gradient-primary">
@@ -50,14 +33,6 @@
                                                 name="password" placeholder="Password">
                                         </div>
 
-                                        @error('password')
-                                            <p>{{ $message }}</p>
-                                        @enderror
-
-                                        @error('username')
-                                            {{ $message }}
-                                        @enderror
-
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login
                                         </button>
@@ -71,15 +46,7 @@
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+    <x-partials._js />
 
 </body>
 
--- resources/views/product-category/form.blade.php
+++ resources/views/product-category/form.blade.php
@@ -1,45 +1,74 @@
+@php
+$url = request()
+    ->route()
+    ->getPrefix();
+$categoryValue = '';
+$descriptionValue = '';
+
+if (count(old()) > 0) {
+    $categoryValue = old('category');
+    $descriptionValue = old('description');
+} elseif (isset($productCategory)) {
+    $categoryValue = $productCategory->category;
+    $descriptionValue = $productCategory->description;
+}
+
+if (isset($productCategory)) {
+    $url = "$url/$productCategory->id";
+}
+
+$routeName = explode(
+    '.',
+    request()
+        ->route()
+        ->getName(),
+);
+$routeType = $routeName[count($routeName) - 1];
+
+if ($routeType === 'add') {
+    $button = [
+        'name' => 'Create',
+        'type' => 'primary',
+    ];
+} elseif ($routeType === 'edit') {
+    $button = [
+        'name' => 'Edit',
+        'type' => 'warning',
+    ];
+}
+@endphp
+
 <x-dashboard>
-    @php
-        $url = request()
-            ->route()
-            ->getPrefix();
-        $categoryValue = '';
-        $descriptionValue = '';
-        
-        if (count(old()) > 0) {
-            $categoryValue = old('category');
-            $descriptionValue = old('description');
-        } elseif (isset($productCategory)) {
-            $categoryValue = $productCategory->category;
-            $descriptionValue = $productCategory->description;
-        }
-        
-        if (isset($productCategory)) {
-            $url = "$url/$productCategory->id";
-        }
-    @endphp
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Buttons</h1>
+    <h1 class="h3 mb-4 text-gray-800">{{ Str::ucfirst($routeType) . " $heading" }}</h1>
 
-    <form class="row" action="{{ url($url) }}" method="post">
-        @isset($productCategory)
-            @method('put')
-        @endisset
-        @csrf
+    <x-partials._form :action="url($url)">
+        <x-slot:method>
+            @isset($productCategory)
+                @method('put')
+            @endisset
+        </x-slot:method>
 
         <div class="col-lg-6 d-flex flex-column">
-            <div class="form-group">
-                <label for="category">Category</label>
-                <input type="text" class="form-control" id="category" name="category" value="{{ $categoryValue }}">
-            </div>
+            @foreach ($formInputs as $input)
+                @php
+                    if ($input['name'] === 'category') {
+                        $value = $categoryValue;
+                    } elseif ($input['name'] === 'description') {
+                        $value = $descriptionValue;
+                    }
+                @endphp
 
-            <div class="form-group">
-                <label for="description">Description</label>
-                <textarea class="form-control" id="description" rows="3" name="description">{{ $descriptionValue }}</textarea>
-            </div>
+                @if ($input['type'] === 'text')
+                    <x-partials._input-text :name="$input['name']" :label="$input['label']" :value="$value" />
+                @elseif($input['type'] === 'textarea')
+                    <x-partials._textarea :name="$input['name']" :label="$input['label']" :value="$value" />
+                @endif
+            @endforeach
 
-            <button type="submit" class="btn btn-warning align-self-end">Primary</button>
+            <button type="submit" class="btn btn-{{ $button['type'] }} align-self-end">{{ $button['name'] }}</button>
         </div>
-    </form>
+    </x-partials._form>
+
 </x-dashboard>
