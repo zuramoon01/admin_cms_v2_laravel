087cb89b1b40eec88be6b9e5a7321f1728c6d550
ZuraMoon <zuramoon01@gmail.com>
2022-09-26 09:59:24 +0700

    Add method store to store voucher

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VoucherRequest;
+use App\Models\Voucher;
 use Illuminate\Http\Request;
 
 class VoucherController extends Controller
@@ -14,5 +16,14 @@ class VoucherController extends Controller
     public function create()
     {
         return view('voucher.form');
+    }
+
+    public function store(VoucherRequest $request)
+    {
+        $validated = $request->validated();
+
+        Voucher::create($validated);
+
+        return to_route('voucher.index.view');
     }
 }
