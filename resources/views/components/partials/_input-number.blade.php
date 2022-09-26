18b3b3e7c9ddcb154470f2b164541dd5acd95389
ZuraMoon <zuramoon01@gmail.com>
2022-09-26 16:33:20 +0700

    Add min value to input partials component

--- resources/views/components/partials/_input-number.blade.php
+++ resources/views/components/partials/_input-number.blade.php
@@ -4,6 +4,6 @@
     @if (!isset($isLabel))
         <label for="{{ $name }}">{{ $label }}</label>
     @endif
-    <input type="number" class="form-control" id="{{ $name }}" name="{{ $name }}"
+    <input type="number" class="form-control" id="{{ $name }}" name="{{ $name }}" min="0"
         value="{{ $value }}" @if (isset($isLabel)) placeholder="{{ $label }}" @endif>
 </div>
