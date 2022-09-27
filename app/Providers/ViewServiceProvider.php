e7e3b954eaf95d22ef5e8df1b19912374ae3e1da
Zura Moon <zuramoon01@gmail.com>
2022-09-27 08:48:31 +0700

    Add dashboar view a composer

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -9,8 +9,8 @@ use App\View\Composers\DashboardComposer;
 use App\View\Composers\AuthorizationComposer;
 use App\View\Composers\ProductCategoryComposer;
 use App\View\Composers\ProductComposer;
-use App\View\Composers\TransactionComposer;
 use App\View\Composers\VoucherComposer;
+use App\View\Composers\TransactionComposer;
 
 class ViewServiceProvider extends ServiceProvider
 {
@@ -31,6 +31,7 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        View::composer('index', DashboardComposer::class);
         View::composer('authorization.index', AuthorizationComposer::class);
         View::composer(['product-category.index', 'product-category.form'], ProductCategoryComposer::class);
         View::composer(['product.index', 'product.form'], ProductComposer::class);
