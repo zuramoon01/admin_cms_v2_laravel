23b71ba5e154bc122675cf4bcc0b1267e8f0844b
ZuraMoon <zuramoon01@gmail.com>
2022-10-03 13:33:26 +0700

    Fix logic where isLabel null in input text components

--- resources/views/components/partials/_input-text.blade.php
+++ resources/views/components/partials/_input-text.blade.php
@@ -5,5 +5,5 @@
         <label for="{{ $name }}">{{ $label }}</label>
     @endif
     <input type="text" class="form-control" id="{{ $name }}" name="{{ $name }}"
-        value="{{ $value }}" @if (!$isLabel) placeholder="{{ $label }}" @endif>
+        value="{{ $value }}" @if (!isset($isLabel)) placeholder="{{ $label }}" @endif>
 </div>
